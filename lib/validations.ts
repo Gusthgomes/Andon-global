8d185019c4400d4aea60c383b337af3656b77715
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-02-18 21:41:26 -0300

    feat: add signIn schema

--- lib/validations.ts
+++ lib/validations.ts
@@ -4,6 +4,7 @@ export const signUpSchema = z.object({
   fullName: z.string().min(3),
   email: z.string().email(),
   password: z.string().min(8),
+  role: z.enum(["COMMON", "ADMIN", "USER", "DPRO", "DALG"]),
 });
 
 export const signInSchema = z.object({
