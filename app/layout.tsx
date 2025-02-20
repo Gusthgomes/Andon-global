07da0fe9bdddd1d5e5e94df2ca8cebd0dc7192ca
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-02-20 20:31:19 -0300

    chore: change app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Andon Global",
+  title: "Andon AMF",
   description: "Gerenciador de todas as linhas de produção",
 };
 
