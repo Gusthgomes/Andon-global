9c525e230a88f5192ca155f7cfe917a5e1325712
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-02-24 20:03:29 -0300

    feat: add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider";
+
 import { Toaster } from "@/components/ui/toaster"
 
 const geistSans = Geist({
@@ -29,8 +31,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main>{children}</main>
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main>{children}</main>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,13 @@ import { auth } from "@/configs/firebaseConfig";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
-import { Link, LogOut, MenuIcon, Ticket } from "lucide-react";
+import { LogOut, MenuIcon, Ticket } from "lucide-react";
 import { Card, CardContent } from "./ui/card";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import SideMenu from "./SideMenu";
+
+import { ThemeToggle } from "./ui/theme/theme-toggle";
+import Link from "next/link";
 
 const Header = () => {
     const [userEmail, setUserEmail] = useState<string | null>(null);
@@ -41,14 +44,16 @@ const Header = () => {
             <CardContent className="p-5 justify-between flex flex-row  items-center">
                 <div className="flex items-center gap-3">
                     <Link href="/">
-                        <Ticket size={30} className="text-white" />
+                        <Ticket size={30} />
                     </Link>
-                    <h1 className="font-bold text-xl font-mono text-black">
+                    <h1 className="font-bold text-xl font-mono">
                         Tickets AMF
                     </h1>
                 </div>
                 <div className="flex items-center justify-between gap-2">
-                    {userEmail ? <p className="text-md font-semibold text-black">Olá {userEmail}</p> : <p>Carregando...</p>}
+                    {userEmail ? <p className="text-md font-semibold">Olá {userEmail}</p> : <p>Carregando...</p>}
+
+                    <ThemeToggle />
 
                     <Sheet>
                         <SheetTrigger asChild>
