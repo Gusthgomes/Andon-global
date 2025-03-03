5c45c7a78d1ec0c8e9704eb7b9f1716d3c9d6a11
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-03-03 11:07:30 -0300

    chore: add title

--- app/(root)/newTicket/page.tsx
+++ app/(root)/newTicket/page.tsx
@@ -1,6 +1,10 @@
 import NewTicket from '@/components/NewTicket'
 import React from 'react'
 
+export const metadata = {
+    title: "Novo ticket",
+}
+
 const page = () => {
     return (
         <NewTicket />
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
-
-
 import React from "react";
 
+export const metadata = {
+  title: "Home Page",
+}
 
 const Home = () => {
 
