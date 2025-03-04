d688ccbaca425592e68aaa3ef0f06398c095d647
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-03-04 14:29:09 -0300

    feat: Showing all tickets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,18 @@
+import TicketList from "@/components/TicketList";
 import React from "react";
 
 export const metadata = {
-  title: "Home Page",
+  title: "Dashboard",
 }
 
 const Home = () => {
 
-  return <div>Home page</div>;
+  return (
+    <div className="px-5 py-6">
+      <h1 className="text-2xl font-bold text-center mb-4">Lista de Tickets</h1>
+      <TicketList />
+    </div>
+  )
 };
 
 export default Home;
