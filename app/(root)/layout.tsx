77130700ac4bddf7d6e6ffa96cc1a2eb3cab74b7
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-03-06 20:46:33 -0300

    refactor: removed auth provider

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,27 @@
-import { AuthProvider } from '@/context/AuthContext'
+"use client";
+
+import { useAuth } from '@/context/AuthContext'
 import Header from '@/components/Header'
 import React, { ReactNode } from 'react'
 
+import { redirect } from 'next/navigation';
+
 const layout = ({ children }: { children: ReactNode }) => {
+
+    const { userData, user } = useAuth();
+
+    if (!user) {
+        return redirect('/sign-in')
+    };
+
+    if (userData?.role === "COMMON") {
+        return redirect('/too-fast')
+    };
+
     return (
         <div className='w-full'>
-            <AuthProvider>
-                <Header />
-                {children}
-            </AuthProvider>
+            <Header />
+            {children}
         </div>
     )
 }
