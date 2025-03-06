0511f849c2d10fce41968914a5e5179d591daf55
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-03-06 20:47:25 -0300

    feat: checking user role to show some routes

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import React from 'react'
+
 import { SheetClose, SheetHeader, SheetTitle } from './ui/sheet'
+
 import Link from 'next/link'
-import { HomeIcon, LogIn, TicketPlus, Ticket } from 'lucide-react'
+
+import { HomeIcon, TicketPlus, Ticket } from 'lucide-react'
+
 import { Button } from './ui/button'
 
+import { useAuth } from '@/context/AuthContext'
+
 const SideMenu = () => {
+
+    const { userData, user } = useAuth()
+
     return (
         <>
             <SheetHeader className='text-left border-b border-solid border-secondary p-5'>
@@ -24,41 +35,36 @@ const SideMenu = () => {
                     </Link>
                 </SheetClose>
 
-                <SheetClose asChild>
-                    <Link href="/myTickets">
-                        <Button
-                            variant="outline"
-                            className="w-full items-start justify-start"
-                        >
-                            <Ticket size={18} className="mr-2" />
-                            Meus tickets
-                        </Button>
-                    </Link>
-                </SheetClose>
+                {userData?.role === "DPRO" && (
+                    <>
+                        <SheetClose asChild>
+                            <Link href="/myTickets">
+                                <Button
+                                    variant="outline"
+                                    className="w-full items-start justify-start"
+                                >
+                                    <Ticket size={18} className="mr-2" />
+                                    Meus tickets
+                                </Button>
+                            </Link>
+                        </SheetClose>
 
-                <SheetClose asChild>
-                    <Link href="/sign-in">
-                        <Button
-                            variant="outline"
-                            className="w-full items-start justify-start"
-                        >
-                            <LogIn size={18} className="mr-2" />
-                            Login
-                        </Button>
-                    </Link>
-                </SheetClose>
+                        <SheetClose asChild>
+                            <Link href="/newTicket">
+                                <Button
+                                    variant="outline"
+                                    className="w-full items-start justify-start"
+                                >
+                                    <TicketPlus size={18} className="mr-2" />
+                                    Cadastrar Ticket
+                                </Button>
+                            </Link>
+                        </SheetClose>
+                    </>
+                )}
 
-                <SheetClose asChild>
-                    <Link href="/newTicket">
-                        <Button
-                            variant="outline"
-                            className="w-full items-start justify-start"
-                        >
-                            <TicketPlus size={18} className="mr-2" />
-                            Cadastrar Ticket
-                        </Button>
-                    </Link>
-                </SheetClose>
+
+
             </div>
         </>
     )
